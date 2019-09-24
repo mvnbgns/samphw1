2c6feb907a9fe755115f28904b1dd6c72e78b3f0
mvnbgns <31599832+mvnbgns@users.noreply.github.com>
2019-09-24 16:17:28 +0800

    Add files via upload

---
+++ samp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Red='\033[0;31m'
+Green='\033[0;32m'
+Blue='\033[0;34m'
+White='\033[1;37m'
+clear
+
+printf "${Red}ENTER USER ID: "
+	read uid
+printf "${Red}ENTER PASSWORD: "
+	read pass
+clear
+printf "${Green}\t\tWELCOME ADMIN!\n"
+
+printf "${Green} 1 Nmap Tools\n 2 Wifite\n 3 Bruteforce\n"
+
+printf "${Blue}MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\n"
+printf "${Blue}MMMMMMMMMM		MMMMMMMMMM\n"
+printf "${Blue}MMMMM[			    ]MMMMM\n"
+printf "${Blue} MMMM[${White}MMMM	          MMMM${Blue}]MMMM\n"
+
